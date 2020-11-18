104235b25d9cd58d766770c057131a7326348d0e
Alan Acuña <alantheace@gmail.com>
2020-11-17 18:27:13 -0600

    Demo app created & configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AboutPage from './pages/AboutPage';
+import HomePage from './pages/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <Switch>
+                    <Route path="/about">
+                        <AboutPage />
+                    </Route>
+                    <Route path="/">
+                        <HomePage />
+                    </Route>
+                    <Redirect to="/"/>
+                </Switch>
+            </main>
+        </>
+    )
 }
 
 export default App;
